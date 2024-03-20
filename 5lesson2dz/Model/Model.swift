5f1f6212b6431d49cac9ad90ccc4a4e29767e58c
aiturann <aiturgannnnnn@gmail.com>
2024-03-20 17:47:20 +0600

    dz2

---
+++ 5lesson2dz/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  5lesson2dz
+//
+//  Created by Aiturgan Kurmanbekova on 20/3/24.
+//
+
+import Foundation
+import UIKit
+
+struct Model {
+    var image: UIImage
+    var title: String
+    var description: String
+}
